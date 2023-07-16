fddf06820faa897af8cccc442eaba3b97c19e4aa
Edy Prasetiyo <edy.prasetiyo@bpkp.go.id>
2023-07-16 23:31:50 +0700

    Pertama

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,30 @@
-import {createRouter,createWebHashHistory} from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router";
 
-import MainPage from '../views/MainPage.vue'
+import MainPage from "../views/MainPage.vue";
+import FormPage from "../views/forms/FormPage.vue";
+import TablePage from "../views/TablePage.vue";
+import General from "../views/uielements/General.vue";
 
-const  router= createRouter({
-    history : createWebHashHistory(),
-    routes : [{
-        path : '/',
-        component : MainPage
-    }]
-})
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      component: MainPage,
+    },
+    {
+      path: "/form",
+      component: FormPage,
+    },
+    {
+      path: "/table",
+      component: TablePage,
+    },
+    {
+      path: "/general",
+      component: General,
+    },
+  ],
+});
 
-export default router
+export default router;
