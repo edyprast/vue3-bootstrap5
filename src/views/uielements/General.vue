351d6663c9c562cc74bcd531da813fe8a6800ad0
Edy Prasetiyo <edy.prasetiyo@bpkp.go.id>
2023-07-16 23:35:04 +0700

    Kedua

---
+++ src/views/uielements/General.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">General</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">General</li>
+        </ol>
+        <ColorsPallete />
+        <AlertCallout />
+
+    </div>
+</template>
+
+<script>
+import ColorsPallete from './ColorsPallete.vue';
+import AlertCallout from './AlertsCallout.vue'
+export default {
+    components: { ColorsPallete, AlertCallout }
+}
+</script>
+
